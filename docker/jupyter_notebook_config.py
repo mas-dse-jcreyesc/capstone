c8223e910fbbf13adc37de2a4e383a621f8eafb8
Juan Reyes <jreyes1108@gmail.com>
2019-01-18 10:53:44 -0800

    ADD WORK FRAMEWORK TO REPO

---
+++ docker/jupyter_notebook_config.py
@@ -0,0 +1,7 @@
+c.NotebookApp.token = ''     # disable authentication
+c.NotebookApp.ip = '0.0.0.0' # listen on all IPs
+c.NotebookApp.allow_root = True
+c.NotebookApp.open_browser = False
+c.NotebookApp.allow_remote_access = True
+c.NotebookApp.iopub_data_rate_limit = 1.0e10
+c.MultiKernelManager.default_kernel_name = 'python3'
