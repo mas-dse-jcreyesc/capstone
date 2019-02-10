96386935f6b01628268c5f59d3b7eee2b516b7bf
jeet.nagda <jeet.nagda@workday.com>
2019-02-10 14:58:40 -0800

    update interfaces and simple text processor

--- api/TextProcessorAbstract.py
+++ api/TextProcessorAbstract.py
@@ -1,21 +1,26 @@
-import abc
+from abc import ABC, abstractmethod
 
-class TextProcessorAbstract(abc.ABCMeta):
+
+class TextProcessorAbstract(ABC):
     """
     Helper Class to clean and process text
     """
-    def clean_text(self, text):
+    @abstractmethod
+    def clean_text(self, text, remove_stopwords=True):
         """
         Cleans the text from english noise
         :param text: input text string
+        :param remove_stopwords: boolean indicating if should remove stopwords
         :return: text: output text string
         """
         pass
 
-    def process_text(self, text):
+    @abstractmethod
+    def process_text(self, text, remove_stopwords=True):
         """
         Returns text in form of vector
         :param text: input text string
+        :param remove_stopwords: boolean indicating if should remove stopwords
         :return: vector representing document text
         """
         pass
